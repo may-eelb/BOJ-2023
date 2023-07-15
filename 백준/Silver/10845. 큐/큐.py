eca5d1d18ba31d585d9e7c6408af0e33bc89f75c
mayeelb <106696686+may-eelb@users.noreply.github.com>
2023-07-16 01:31:06 +0900

    [Silver IV] Title: 큐, Time: 176 ms, Memory: 117272 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,34 @@
+
+import sys
+queue = []
+
+N = int(sys.stdin.readline())
+for i in range(N):
+    temp = list(sys.stdin.readline().split())
+    if (len(temp) == 2):  ## push
+        queue.append(temp[1])
+
+    elif temp[0] == "pop":
+        if (len(queue) == 0):
+            print(-1)
+        else:
+            print(queue[0])
+            queue.pop(0)
+
+    elif temp[0] == "size":
+        print(len(queue))
+    elif temp[0] == "empty":
+        if (len(queue)) == 0:
+            print(1)
+        else:
+            print(0)
+    elif temp[0] == "front":
+        if (len(queue)) != 0:
+            print(queue[0])
+        else :
+            print(-1)
+    else:
+        if (len(queue)) != 0:
+            print(queue[-1])
+        else :
+            print(-1)
