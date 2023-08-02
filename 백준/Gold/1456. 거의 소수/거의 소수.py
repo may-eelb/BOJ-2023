ab6b5396f8c68be4d338c115112bd0beb72c8c68
mayeelb <106696686+may-eelb@users.noreply.github.com>
2023-08-02 19:07:23 +0900

    [Gold V] Title: 거의 소수, Time: 5084 ms, Memory: 429968 KB -BaekjoonHub

---
+++ 백준/Gold/1456. 거의 소수/거의 소수.py
@@ -0,0 +1,29 @@
+from sys import stdin
+input = stdin.readline
+import math
+
+Min, Max = map(int, input().split())
+A = [0] * ( 10000001 )
+
+for i in range(2, len(A)):
+    A[i] = i
+
+
+# 소수 구하기
+for i in range(2, int(math.sqrt(len(A)) + 1)):
+    if A[i] == 0:
+        continue
+    for j in range(i + i, len(A), i):
+        A[j] = 0
+
+count = 0
+
+for i in range(2, 10000001):
+    if A[i] != 0:
+        temp = A[i]
+        while A[i] * temp <= Max :
+            if A[i] * temp >= Min :
+                count += 1
+            temp = temp * A[i]
+
+print(count)
