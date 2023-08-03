d74622fca66ec5945b7e7bf46758e83d9f325f24
mayeelb <106696686+may-eelb@users.noreply.github.com>
2023-08-04 07:51:36 +0900

    [Silver II] Title: 최대 힙, Time: 128 ms, Memory: 37180 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -0,0 +1,16 @@
+from sys import stdin
+input = stdin.readline
+import heapq
+
+pq = []
+n = int(input())
+for _ in range (n):
+    temp = int(input())
+    if temp == 0:
+        if pq :
+            print(-heapq.heappop(pq))
+        else :
+            print(0)
+    else:
+        heapq.heappush(pq, -temp)
+
